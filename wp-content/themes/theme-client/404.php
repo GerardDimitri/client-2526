6f59a82111aa6888253db354cd454e51c3dbc9e8
Gerard Dimitri <dimitri.gerard@student.hepl.be>
2026-04-22 15:14:55 +0200

    feat: add authentification

---
+++ wp-content/themes/theme-client/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+    <section>
+        <h2>404 Page non trouvée</h2>
+        <p>Cette page n'existe pas</p>
+        <p>Retour à <a href="/" title="Vers la page d'accueil">la page d'accueil</a></p>
+    </section>
+
+<?php get_footer(); ?>
